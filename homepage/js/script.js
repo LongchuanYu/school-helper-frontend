ffd413424d3e98123c00c308d14ace40f4c1d82f
LongchuanYu <remly@qq.com>
2022-05-18 11:14:50 +0800

    add news

--- homepage/js/script.js
+++ homepage/js/script.js
@@ -1,14 +1,5 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  },
-  methods: {
-    initQrCode() {
-      $('[data-toggle="tooltip"]').tooltip(); 
-    }
-  },
-  mounted() {
-    this.initQrCode();
-  }
-})
+function initQrCode() {
+  $('[data-toggle="tooltip"]').tooltip(); 
+}
+
+initQrCode();
